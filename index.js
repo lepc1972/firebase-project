4a4c670764c3f6e17201d365a268a815ce401cd7
lepc1972 <githublepc@gmail.com>
2024-09-09 09:04:02 -0500

    index

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+when you clicked aadd to cart button , what ever is writen
+in the input field should be consolo logged
+*/
+
+/*
+When the "Add to Cart" button is clicked, the value entered
+in the input field should be logged to the console.
+*/
+
+// Get references to the input field and button
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+
+addButtonEl.addEventListener("click", function() {
+        console.log("works")
+}
+
+)
+
