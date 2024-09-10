2dca730cc976baca413392612c98fc87e1cdfea1
lepc1972 <githublepc@gmail.com>
2024-09-09 23:26:08 -0500

    firebase working

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ When the "Add to Cart" button is clicked, the value entered
 in the input field should be logged to the console.
 */
 
-// Get references to the input field and button
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+        databaseURL: "https://shopping-reminder-c8e69-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app);
+const moviesInDB = ref(database, "a little bit of everything");
+console.log(database)
+
+console.log(app)
+
 const inputFieldEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
 
 addButtonEl.addEventListener("click", function() {
         let inputValue = inputFieldEl.value
+        push(moviesInDB, inputValue)
         console.log(inputValue)
 
 }
